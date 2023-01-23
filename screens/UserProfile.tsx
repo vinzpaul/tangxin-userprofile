82d56d80cbc93d0a14e86d338bfe1f2a62d1f8a0
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-01-23 20:56:45 +0800

    Adjusted container margins

--- screens/UserProfile.tsx
+++ screens/UserProfile.tsx
@@ -29,136 +29,160 @@ const UserProfile = () => {
           // backgroundColor: "#FFFFFF",
         }}
       >
-        <ScrollView style={{ flex: 1 }}>
-          <View
-            style={{
-              display: "flex",
-              alignItems: "flex-end",
-              padding: 10,
-              flex: 1,
-            }}
-          >
-            <EvilIcons name="gear" size={28} color="white" />
-          </View>
-          <View
-            style={{
-              backgroundColor: "#262632",
-              borderRadius: 10,
-              height: 100,
-              flex: 1,
-            }}
-          >
-            <View style={{ flexDirection: "row", padding: 10 }}>
-              <Image
-                style={{ width: 40, height: 40, borderRadius: 20 }}
-                source={require("../assets/profilePhoto.jpg")}
-              />
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  paddingLeft: 8,
-                }}
-              >
-                <View>
-                  <Text
-                    style={{ color: "white", fontSize: 15, fontWeight: "600" }}
-                  >
-                    受伤的期待
-                  </Text>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      paddingHorizontal: gap / -2,
-                    }}
-                  >
-                    <Text
-                      style={{
-                        color: "white",
-                        fontSize: 11,
-                        // marginHorizontal: gap / 2,
-                      }}
-                    >
-                      金币: 0
-                    </Text>
-                    <Text
-                      style={{
-                        color: "white",
-                        fontSize: 11,
-                        marginHorizontal: gap / 2,
-                      }}
-                    >
-                      观影券: 0
-                    </Text>
-                    <Text
-                      style={{
-                        color: "white",
-                        fontSize: 11,
-                        marginHorizontal: gap / 2,
-                      }}
-                    >
-                      免费观看 : 0
-                    </Text>
-                  </View>
-                </View>
+        <ScrollView style={{ flex: 1, paddingVertical: gap / -2 }}>
+          <View style={{ marginVertical: gap / 1 }}>
+            <View
+              style={{
+                display: "flex",
+                alignItems: "flex-end",
+                padding: 10,
+                flex: 1,
+              }}
+            >
+              <EvilIcons name="gear" size={28} color="white" />
+            </View>
+            <View
+              style={{
+                backgroundColor: "#262632",
+                borderRadius: 10,
+                height: 100,
+                flex: 1,
+              }}
+            >
+              <View style={{ flexDirection: "row", padding: 10 }}>
+                <Image
+                  style={{ width: 40, height: 40, borderRadius: 20 }}
+                  source={require("../assets/profilePhoto.jpg")}
+                />
                 <View
                   style={{
                     flexDirection: "row",
                     alignItems: "center",
-                    position: "absolute",
-                    left: 220,
+                    paddingLeft: 8,
                   }}
                 >
-                  <Text
+                  <View>
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 15,
+                        fontWeight: "600",
+                      }}
+                    >
+                      受伤的期待
+                    </Text>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        paddingHorizontal: gap / -2,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          color: "white",
+                          fontSize: 11,
+                          // marginHorizontal: gap / 2,
+                        }}
+                      >
+                        金币: 0
+                      </Text>
+                      <Text
+                        style={{
+                          color: "white",
+                          fontSize: 11,
+                          marginHorizontal: gap / 2,
+                        }}
+                      >
+                        观影券: 0
+                      </Text>
+                      <Text
+                        style={{
+                          color: "white",
+                          fontSize: 11,
+                          marginHorizontal: gap / 2,
+                        }}
+                      >
+                        免费观看 : 0
+                      </Text>
+                    </View>
+                  </View>
+                  <View
                     style={{
-                      color: "white",
-                      fontSize: 13,
-                      marginRight: 5,
+                      flexDirection: "row",
+                      alignItems: "center",
+                      position: "absolute",
+                      left: 220,
                     }}
                   >
-                    主页
-                  </Text>
-                  <FontAwesome5 name="angle-right" size={15} color="white" />
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 13,
+                        marginRight: 5,
+                      }}
+                    >
+                      主页
+                    </Text>
+                    <FontAwesome5 name="angle-right" size={15} color="white" />
+                  </View>
                 </View>
               </View>
+              <View
+                style={{
+                  borderTopColor: "white",
+                  borderTopWidth: StyleSheet.hairlineWidth,
+                  marginHorizontal: 10,
+                }}
+              ></View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  marginHorizontal: 50,
+                  paddingTop: 5,
+                }}
+              >
+                <Text
+                  style={{ color: "white", fontSize: 13, fontWeight: "600" }}
+                >
+                  动态 0
+                </Text>
+                <Text
+                  style={{ color: "white", fontSize: 13, fontWeight: "600" }}
+                >
+                  关注 0
+                </Text>
+                <Text
+                  style={{ color: "white", fontSize: 13, fontWeight: "600" }}
+                >
+                  粉丝 0
+                </Text>
+              </View>
             </View>
-            <View
-              style={{
-                borderTopColor: "white",
-                borderTopWidth: StyleSheet.hairlineWidth,
-                marginHorizontal: 10,
-              }}
-            ></View>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                marginHorizontal: 50,
-                paddingTop: 5,
-              }}
-            >
-              <Text style={{ color: "white", fontSize: 13, fontWeight: "600" }}>
-                动态 0
-              </Text>
-              <Text style={{ color: "white", fontSize: 13, fontWeight: "600" }}>
-                关注 0
-              </Text>
-              <Text style={{ color: "white", fontSize: 13, fontWeight: "600" }}>
-                粉丝 0
-              </Text>
+          </View>
+
+          <View style={{ marginVertical: gap / 1 }}>
+            <View style={{ backgroundColor: "brown" }}>
+              <Text>Second Container</Text>
             </View>
           </View>
-          <View style={{ backgroundColor: "brown" }}>
-            <Text>Second Container</Text>
+
+          <View style={{ marginVertical: gap / 1 }}>
+            <View style={{ backgroundColor: "brown" }}>
+              <Text>Third Container</Text>
+            </View>
           </View>
-          <View style={{ backgroundColor: "brown" }}>
-            <Text>Third Container</Text>
+
+          <View style={{ marginVertical: gap / 1 }}>
+            <View style={{ backgroundColor: "brown" }}>
+              <Text>Fourth Container</Text>
+            </View>
           </View>
-          <View style={{ backgroundColor: "brown" }}>
-            <Text>Fourth Container</Text>
-          </View>
-          <View style={{ backgroundColor: "brown" }}>
-            <Text>Fifth Container</Text>
+
+          <View style={{ marginVertical: gap / 1 }}>
+            <View style={{ backgroundColor: "brown" }}>
+              <Text>Fifth Container</Text>
+            </View>
           </View>
         </ScrollView>
       </View>
