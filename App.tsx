6a90b93b2861939595e750e1536104082a82ee5e
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-01-27 16:35:23 +0800

    Fixed CSS Structure for ProfilePhoto.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
-import React, {useState} from "react";
-import {StatusBar} from "expo-status-bar";
-import {StyleSheet, Text, View, SafeAreaView} from "react-native";
+import React from "react";
+import {StyleSheet} from "react-native";
 
-import UserProfile from "./screens/UserProfile";
-import SettingsIcon from "./screens/UserProfileComponents/SettingsIcon";
+//Navigation
 import {NavigationContainer, DefaultTheme} from "@react-navigation/native";
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
-import {Transition} from 'react-native-reanimated';
-import ProfilePhoto from "./screens/UserProfileComponents/SettingsComponents/ProfilePhoto";
-
 const myTheme = {
     dark: true,
-    // backgroundColor: "#191d26",
     colors: {
         ...DefaultTheme.colors,
         primary: 'rgb(255, 45, 85)',
@@ -24,6 +18,10 @@ const myTheme = {
 //Redux
 import {Provider} from 'react-redux';
 import {store} from './redux/store/store';
+
+import UserProfile from "./screens/UserProfile";
+import SettingsIcon from "./screens/UserProfileComponents/SettingsIcon";
+import ProfilePhoto from "./screens/UserProfileComponents/SettingsComponents/ProfilePhoto";
 import PetName from "./screens/UserProfileComponents/SettingsComponents/PetName";
 import BindRequest from "./screens/UserProfileComponents/SettingsComponents/BindRequest";
 import Introduction from "./screens/UserProfileComponents/SettingsComponents/Introduction";
--- screens/UserProfileComponents/SettingsComponents/ProfilePhoto.tsx
+++ screens/UserProfileComponents/SettingsComponents/ProfilePhoto.tsx
@@ -1,25 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import {
-    StyleSheet,
     Text,
     View,
     ScrollView,
-    SafeAreaView,
-    SectionList,
-    StatusBar,
     Dimensions,
     Image,
-    FlatList, TouchableOpacity, TextInput
+    TouchableOpacity, StyleSheet
 } from "react-native";
+
 import {useNavigation} from "@react-navigation/native";
 import {StackNavigationProp} from "@react-navigation/stack";
+
 import {RootStackParamList} from "../../UserProfile";
+
 import {AntDesign} from "@expo/vector-icons";
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 //Redux
-import { useSelector, useDispatch} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 
 const ProfilePhoto = () => {
 
@@ -33,16 +32,19 @@ const ProfilePhoto = () => {
     const girls = useSelector((store) => store['models'].current);
     const selectedImage = useSelector((store) => store['models'].selectedImage);
 
+    //Changes Main Profile Photo placeholder on hover
     const tempMainImage = (newImage) => {
         setMainImage(newImage);
     }
+
+    //Stores image in API AsyncStorage
     const changeMainImage = async (newImage) => {
         setMainImage(newImage);
         try {
             const jsonValue = JSON.stringify(newImage);
             await AsyncStorage.setItem('mainImage', jsonValue);
 
-            navigation.navigate('Settings',{mainImage: mainImage});
+            navigation.navigate('Settings', {mainImage: mainImage});
         } catch (e) {
             console.log(e)
         }
@@ -52,48 +54,51 @@ const ProfilePhoto = () => {
         async function getData() {
             try {
                 const value = await AsyncStorage.getItem('mainImage');
-                if(value !== null) {
+                if (value !== null) {
                     setMainImage(value);
                 }
-            } catch(e) {
+            } catch (e) {
                 console.log(e)
             }
         }
+
         getData();
     }, []);
 
     return (
-        <ScrollView style={{flex: 1, maxHeight: Dimensions.get("window").height,
-            marginVertical: 20}}>
+        <ScrollView style={styles.container}>
 
             {/*Title and Back Button  */}
-            <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginVertical: 10, backgroundColor: '#262632', height: 50}}>
-                <View style={{position: 'absolute', left: 5}}>
-                    <TouchableOpacity onPress={()=>navigation.navigate('Settings')}>
-                        <AntDesign name="left" size={24} color="white" />
+            <View style={styles.titleAndBackBtn}>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                        <AntDesign name="left" size={24} color="white"/>
                     </TouchableOpacity>
                 </View>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <Text style={{color: '#FFFFFF', textAlign: 'center', fontSize: 20}}>修改头像</Text>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.textDesign}>修改头像</Text>
                 </View>
-                <TouchableOpacity style={{position: 'absolute', right: 23}} onPress={() =>{changeMainImage(mainImage); dispatch({type: 'SET_IMAGE', payload: mainImage})}}>
-                    <Text style={{color: '#FF474E'}}>完成</Text>
+                <TouchableOpacity style={styles.acceptBtn} onPress={() => {
+                    changeMainImage(mainImage);
+                    dispatch({type: 'SET_IMAGE', payload: mainImage})
+                }}>
+                    <Text style={styles.acceptBtnColor}>完成</Text>
                 </TouchableOpacity>
             </View>
 
             {/*Main Profile Photo*/}
-            <View style={{alignItems: 'center', marginBottom: 20 }}>
+            <View style={styles.mainPhotoContainer}>
                 {girls.map((girl, index) => (
-                    <Image key={girl} source={mainImage} style={{ flex: 1, width: 360, height: 360, borderRadius: 200 }} />
+                    <Image key={girl} source={mainImage} style={styles.mainPhoto}/>
                 ))}
             </View>
 
             {/*Model List*/}
-            <View style={{flexDirection: 'row',flexWrap: 'wrap', justifyContent: 'space-between'}}>
+            <View style={styles.modelContainer}>
                 {models.map((model, index) => (
-                    <View key={model} >
+                    <View key={model}>
                         <TouchableOpacity onPress={() => tempMainImage(model)}>
-                            <Image source={model} style={{  width: 90, height: 90, borderRadius: 50 }}/>
+                            <Image source={model} style={styles.modelPhotos}/>
                         </TouchableOpacity>
                     </View>
                 ))}
@@ -101,5 +106,47 @@ const ProfilePhoto = () => {
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, maxHeight: Dimensions.get("window").height,
+        marginVertical: 20
+    },
+    titleAndBackBtn: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 10,
+        backgroundColor: '#262632',
+        height: 50
+    },
+    btnContainer: {
+        position: 'absolute', left: 5
+    },
+    titleContainer: {
+        flexDirection: 'row', alignItems: 'center'
+    },
+    textDesign: {
+        color: '#FFFFFF', textAlign: 'center', fontSize: 20
+    },
+    acceptBtn: {
+        position: 'absolute', right: 23
+    },
+    acceptBtnColor: {
+        color: '#FF474E'
+    },
+    mainPhotoContainer: {
+        alignItems: 'center', marginBottom: 20
+    },
+    mainPhoto: {
+        flex: 1, width: 360, height: 360, borderRadius: 200
+    },
+    modelContainer: {
+        flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'
+    },
+    modelPhotos: {
+        width: 90, height: 90, borderRadius: 50
+    }
+});
 
 export default ProfilePhoto;
